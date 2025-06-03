e3609f76e1fd76f0fc59d2149aa2bf1d19f62e9e
Anushka Singh <anushka.singh.civ22@itbhu.ac.in>
2025-06-03 16:04:02 +0530

    react router 1st commit

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import { useState } from 'react'
-import  Header  from './components/Header/Header'
-import  Home  from './components/Home/Home'
-import  Footer  from './components/Footer/Footer'
-import  Contact  from './components/Contact/Contact'
-import './App.css'
+import React from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-
-     <Header />
-     <Home />
-     <Contact />
-     <Footer />
-
+      <Header />
+      <Home />
+      <Contact />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
